1334ccb96494a3e283f9ff33cb7c7ad88dfba921
mdmohsin1985 <mohsinsang@gmail.com>
2026-05-05 17:49:17 +0530

    added new files

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,10 @@
 describe('Login Flow', () => {
   it('should login successfully with valid credentials', () => {
-    cy.visit('https://www.github.com/');
-    cy.get('.HeaderMenu-link--sign-in ').click({force: true});
-    cy.log('Password is:', Cypress.env('password'));
+    cy.visit('https://www.apsrtconline.in/');
+    cy.get("[id='bookingsForm'] [id='home-bot'] ul li").eq(0).click();
+    cy.get(".ui-state-active").click();
+    cy.get(".chkavailabilityBtn").click();
+    cy.log("Password is:", Cypress.env('password'));
 
-    cy.get('[id="login_field"]').type('mdmohsin1985');
-    cy.get('[id="password"]').type(Cypress.env('password'));
-    cy.get('js-sign-in-button').click();
-    cy.url().should('include', '/MdMohsin1985');
   });
 });
