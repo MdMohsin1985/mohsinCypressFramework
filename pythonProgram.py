7e58e2d1869e0f9e7e3fd0e2c3e3d88ac16889f8
mdmohsin1985 <mohsinsang@gmail.com>
2026-05-20 17:16:40 +0530

    add java js and python files to check execution

---
+++ pythonProgram.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+print("Prime numbers are below ", num, "are :")
+
+for n in range(2, num + 1):
+    is_prime = True
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(n)
